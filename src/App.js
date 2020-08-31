ac81bc8c34b5fae4ff27f2a0fb1edea3dff97623
Thong Nguyen <nanolove95@gmail.com>
2020-08-31 13:26:26 +0700

    :construction: Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+/*
+ * This is the entry point component of this project. You can change the below exported default App component to any of
+ * the prebuilt landing page components by uncommenting their import and export lines respectively.
+ * See one of the landing page components to better understand how to import and render different components (Always
+ * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
+ * disable the animation by using the disabled prop.
+ *
+ * The App component below is using React router to render the landing page that you see on the live demo website
+ * and the component previews.
+ *
+ */
+
+/* Use AnimationRevealPage as a wrapper component for your pages if you are custom building it */
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
+// import ServiceLandingPage from "demos/ServiceLandingPage.js";
+// import HostingCloudLandingPage from "demos/HostingCloudLandingPage.js";
+
+/* Inner Pages */
+// import LoginPage from "pages/Login.js";
+// import SignupPage from "pages/Signup.js";
+// import PricingPage from "pages/Pricing.js";
+// import AboutUsPage from "pages/AboutUs.js";
+// import ContactUsPage from "pages/ContactUs.js";
+// import BlogIndexPage from "pages/BlogIndex.js";
+// import TermsOfServicePage from "pages/TermsOfService.js";
+// import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  // return <AnimationRevealPage disabled></AnimationRevealPage>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <RestaurantLandingPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+// export default EventLandingPage;
+// export default HotelTravelLandingPage;
+// export default AgencyLandingPage;
+// export default SaaSProductLandingPage;
+// export default RestaurantLandingPage;
+// export default ServiceLandingPage;
+// export default HostingCloudLandingPage;
+
+// export default LoginPage;
+// export default SignupPage;
+// export default PricingPage;
+// export default AboutUsPage;
+// export default ContactUsPage;
+// export default BlogIndexPage;
+// export default TermsOfServicePage;
+// export default PrivacyPolicyPage;
+
+// export default MainLandingPage;
