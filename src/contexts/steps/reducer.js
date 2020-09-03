4cdde76e06e66edd3e74a0ddae972a59a7ba11d8
Thong Nguyen <nanolove95@gmail.com>
2020-09-03 15:50:06 +0700

    :sparkles: Add shippingInfo step

--- src/contexts/steps/reducer.js
+++ src/contexts/steps/reducer.js
@@ -4,17 +4,26 @@ export default (state, action) => {
   switch (action.type) {
     case actions.nextStep: {
       return {
+        ...state,
         currentStep: state.currentStep + 1,
       };
     }
     case actions.previousStep: {
       return {
+        ...state,
         currentStep: state.currentStep - 1,
       };
     }
     case actions.gotoStep: {
       return {
+        ...state,
         currentStep: action.value,
+      };
+    }
+    case actions.requestNextStep: {
+      return {
+        ...state,
+        requestNextStep: true,
       };
     }
     default: {
